45c37df2a428a1c61f43f1980e54673ea5d96d55
mel dalisay <rommelmdalisay@gmail.com>
2025-07-16 14:29:12 +0800

    variable

--- sample.py
+++ sample.py
@@ -1,4 +1,33 @@
-print("hello world")
-room = 12.50
-print(type(room))
-print('I am staying in room number', room)
+# print("hello world")
+# room = 12.50
+# print(type(room))
+# print('I am staying in room number', room)
+
+
+# first_name = int(input('Enter your first name: '))
+# Get the user's last name.
+# last_name = int(input('Enter your last name: '))
+
+# Print a greeting to the user.
+# print('Hello', first_name, last_name)
+
+
+
+# sum = first_name + last_name
+# print(sum)
+
+
+hours = int(input('How many hours did you work? '))
+print(hours)
+
+name = input('What is your name? ')
+age = int(input('What is your age? '))
+income = float(input('What is your income? '))
+
+print('Here is the data you entered:')
+print('Name:', name)
+print('Age:', age)
+print('Income:', income)
+
+
+
